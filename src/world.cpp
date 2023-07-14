c92686fb0c23cae4c4351c9651ea16c5ef7f33b0
Nasar <196.nasar@gmail.com>
2023-07-14 22:50:20 +0200

    Win and lose conds

--- src/world.cpp
+++ src/world.cpp
@@ -13,16 +13,17 @@ void World::initializeBullets(int nBullets)
 {
 	for (int i = 0; i < nBullets; i++)
 	{
-		EntityBullet* bullet = new EntityBullet();
+		EntityBullet *bullet = new EntityBullet();
 		bulletBuffer.push_back(bullet);
 	}
 }
 
-void World::shootBullet(Vector3 direction, EntityEnemy* source) 
+void World::shootBullet(Vector3 direction, EntityEnemy *source)
 {
-	//EntityBullet* bullet = bulletBuffer[0];
+	// EntityBullet* bullet = bulletBuffer[0];
 
-	auto it = std::find_if(bulletBuffer.begin(), bulletBuffer.end(), [&](const auto& ptr) {	return ptr == bullet; });
+	auto it = std::find_if(bulletBuffer.begin(), bulletBuffer.end(), [&](const auto &ptr)
+						   { return ptr == bullet; });
 	if (source)
 	{
 		bullet->enemySource = source;
@@ -36,10 +37,10 @@ void World::shootBullet(Vector3 direction, EntityEnemy* source)
 	bullet->activate();
 	Vector3 pos = Game::instance->camera->eye;
 	bullet->model.setTranslation(pos.x, pos.y, pos.z);
-	//bullet.model.setTranslation(position.x, position.y, position.z);
+	// bullet.model.setTranslation(position.x, position.y, position.z);
 }
 
-void World::stashBullet(EntityBullet* bullet)
+void World::stashBullet(EntityBullet *bullet)
 {
 }
 
@@ -47,13 +48,13 @@ void World::loadRooms()
 {
 	for (int i = 0; i < ROOMTYPES; i++)
 	{
-		EntityMeshRoom* room = new EntityMeshRoom(false);
+		EntityMeshRoom *room = new EntityMeshRoom(false);
 		room->parseScene(this->roomTypeNames[i]);
 		this->roomTypes.push_back(room);
 	}
 }
 
-//void World::randomizeMap()
+// void World::randomizeMap()
 //{
 //	// Seed the random number generator
 //	std::srand(std::time(0));
@@ -66,7 +67,7 @@ void World::loadRooms()
 //			if (i == 0 && j == 0)
 //			{
 //				roomType = 0;
-//			} 
+//			}
 //			else
 //			{
 //				roomType = std::rand() % 3;
@@ -79,11 +80,11 @@ void World::loadRooms()
 //			mapGrid[i * GRIDHEIGHT + j] = room;
 //		}
 //	}
-//}
+// }
 
-void World::placeRoom(int roomID, int roomType) 
+void World::placeRoom(int roomID, int roomType)
 {
-	EntityMeshRoom* room = new EntityMeshRoom(true);
+	EntityMeshRoom *room = new EntityMeshRoom(true);
 	room->parseScene(roomTypeNames[roomType]);
 	int i = roomID % GRIDHEIGHT;
 	int j = roomID / GRIDHEIGHT;
@@ -98,18 +99,24 @@ void World::placeRoom(int roomID, int roomType)
 
 void World::tryNeighbour(int roomID)
 {
-	if (roomID < GRIDHEIGHT || roomID > GRIDHEIGHT * (GRIDHEIGHT - 1) || roomID % GRIDHEIGHT == 0 || (roomID + 1) % GRIDHEIGHT == 0) return; // edges must be empty
-	if (mapGrid[roomID]) return; // already a room
+	if (roomID < GRIDHEIGHT || roomID > GRIDHEIGHT * (GRIDHEIGHT - 1) || roomID % GRIDHEIGHT == 0 || (roomID + 1) % GRIDHEIGHT == 0)
+		return; // edges must be empty
+	if (mapGrid[roomID])
+		return; // already a room
 	int neighbourNeighbours = 0;
 	for (int i = 0; i < 4; i++) // count Neighbour Neighbours
 	{
-		if (mapGrid[roomID + neightbourOps[i]]) neighbourNeighbours += 1;
+		if (mapGrid[roomID + neightbourOps[i]])
+			neighbourNeighbours += 1;
 	}
-	if (neighbourNeighbours > 2) return; // if more than two neighbours continue
-	if (placedRooms == totalRooms) return; // All rooms placed
-	if (std::rand() % 2 == 1) return; // 50% of filling the room
+	if (neighbourNeighbours > 2)
+		return; // if more than two neighbours continue
+	if (placedRooms == totalRooms)
+		return; // All rooms placed
+	if (std::rand() % 2 == 1)
+		return;							// 50% of filling the room
 	placeRoom(roomID, std::rand() % 3); // place room
-	
+
 	for (int i = 0; i < 4; i++)
 	{
 		tryNeighbour(roomID + neightbourOps[i]);
@@ -135,11 +142,11 @@ void World::randomLoad()
 
 void World::setTestRooms()
 {
-	vt<EntityMeshRoom*> gridrow;
+	vt<EntityMeshRoom *> gridrow;
 	for (int i = 0; i < ROOMTYPES; i++)
 	{
 		Vector3 roomPosition = Vector3(0.0f, .0f, (i + 0) * 16.0f);
-		EntityMeshRoom* room = roomTypes[i];
+		EntityMeshRoom *room = roomTypes[i];
 		room->model.setTranslation(roomPosition.x, roomPosition.y, roomPosition.z);
 		mapGrid[i] = room;
 	}
@@ -149,36 +156,40 @@ void World::update()
 {
 }
 
-int World::countRoomNeighbours(EntityMeshRoom* room)
+int World::countRoomNeighbours(EntityMeshRoom *room)
 {
 	int neighbourNeighbours = 0;
 	for (int i = 0; i < 4; i++) // count Neighbour Neighbours
 	{
-		if (mapGrid[room->roomID + neightbourOps[i]]) neighbourNeighbours += 1;
+		if (mapGrid[room->roomID + neightbourOps[i]])
+			neighbourNeighbours += 1;
 	}
 	return neighbourNeighbours;
 }
 
 void World::setRoomClearStatus(int roomID)
 {
-	if (mapGrid[roomID]) mapGrid[roomID]->setClearStatus();
+	if (mapGrid[roomID])
+		mapGrid[roomID]->setClearStatus();
 	for (int i = 0; i < 4; i++) // count Neighbour Neighbours
 	{
-		EntityMeshRoom* neighbour = mapGrid[roomID + neightbourOps[i]];
-		if (neighbour) neighbour->openDoor(static_cast<Directions>((i + 2) % 4));
+		EntityMeshRoom *neighbour = mapGrid[roomID + neightbourOps[i]];
+		if (neighbour)
+			neighbour->openDoor(static_cast<Directions>((i + 2) % 4));
 	}
 }
 
-EntityMeshRoom* World::getRoom(int x, int y)
+EntityMeshRoom *World::getRoom(int x, int y)
 {
 	return mapGrid[x * GRIDHEIGHT + y];
 }
 
 void World::placeRoomsDoors()
 {
- 	EACH(room, mapGrid)
+	EACH(room, mapGrid)
 	{
-		if (!room) continue;
+		if (!room)
+			continue;
 		int neighbourNeighbours = 0;
 		int roomID = room->roomID;
 		bool isExternal = false;
@@ -186,8 +197,9 @@ void World::placeRoomsDoors()
 		{
 			bool isExternal = true;
 			int neoghbourID = room->roomID + neightbourOps[direction];
-			if (mapGrid[neoghbourID]) isExternal = false;
-			EntityDoor* door = new EntityDoor(static_cast<Directions>(direction), isExternal);
+			if (mapGrid[neoghbourID])
+				isExternal = false;
+			EntityDoor *door = new EntityDoor(static_cast<Directions>(direction), isExternal);
 			room->children.push_back(door);
 			door->parent = room;
 			room->roomDoors.push_back(door);
@@ -197,10 +209,9 @@ void World::placeRoomsDoors()
 
 void World::render()
 {
-
 }
 
-//void World::stashBullet(EntityBullet* bullet)
+// void World::stashBullet(EntityBullet* bullet)
 //{
 //	// Find the position of the desired pointer in vectorA
 //	auto it = std::find_if(activeBullets.begin(), activeBullets.end(), [&](const auto& ptr) {
@@ -212,9 +223,9 @@ void World::render()
 //		bulletBuffer.emplace_back(std::move(*it));
 //		activeBullets.erase(it); // Remove the pointer from vectorA
 //	}
-//}
+// }
 //
-//void World::shootBullet(Vector3 direction, EntityEnemy* source)
+// void World::shootBullet(Vector3 direction, EntityEnemy* source)
 //{
 //	EntityBullet* bullet = bulletBuffer[0];
 //	auto it = std::find_if(activeBullets.begin(), activeBullets.end(), [&](const auto& ptr) {
@@ -229,4 +240,21 @@ void World::render()
 //		activeBullets.push_back(std::move(*it));
 //		bulletBuffer.erase(it);
 //	}
-//}
+// }
+
+bool World::CheckWinCondition()
+{
+	EACH(room, mapGrid)
+	{
+		if (!room)
+			continue;
+		if (!room->isCleared)
+			return false;
+	}
+	return true;
+}
+
+bool World::CheckLoseCondition()
+{
+	return Game::instance->player->health <= 0;
+}
--- src/world.h
+++ src/world.h
@@ -22,61 +22,63 @@ class EntityBullet;
 class World
 {
 public:
-    World();
+	World();
 
 	int totalRooms = 1;
 	int placedRooms = 0;
 
-    char* roomTypeNames[ROOMTYPES] = { "roomBasic", "roomDiamond", "roomSquare" };
+	char *roomTypeNames[ROOMTYPES] = {"roomBasic", "roomDiamond", "roomSquare"};
 
-    vt<EntityMeshRoom *> roomTypes;
-    vt<EntityMeshRoom*> mapGrid{ GRIDWIDTH * GRIDHEIGHT };
-	vt<int> neightbourOps = { GRIDWIDTH, 1, -GRIDWIDTH, -1 };
+	vt<EntityMeshRoom *> roomTypes;
+	vt<EntityMeshRoom *> mapGrid{GRIDWIDTH * GRIDHEIGHT};
+	vt<int> neightbourOps = {GRIDWIDTH, 1, -GRIDWIDTH, -1};
 
-	vt<EntityEnemy*> activeEnemies;
+	vt<EntityEnemy *> activeEnemies;
 
-	vt<EntityBullet*> bulletBuffer;
-	EntityBullet* bullet;
-	//vt<EntityBullet*> activeBullets;
+	vt<EntityBullet *> bulletBuffer;
+	EntityBullet *bullet;
+	// vt<EntityBullet*> activeBullets;
 
-	vt<EntityMesh*> ceiling;
-	vt<EntityMesh*> floor;
-	vt<EntityMesh*> pilarEast;
-	vt<EntityMesh*> pilarWest;
-	vt<EntityMesh*> pilarSouth;
-	vt<EntityMesh*> pilarNorth;
-	vt<EntityMesh*> bigaEast;
-	vt<EntityMesh*> bigaWest;
-	vt<EntityMesh*> bigaSouth;
-	vt<EntityMesh*> bigaNorth;
-	vt<EntityMesh*> wallEast;
-	vt<EntityMesh*> wallWest;
-	vt<EntityMesh*> wallSouth;
-	vt<EntityMesh*> wallNorth;
-	vt<EntityMesh*> midCube;
-	vt<EntityMesh*> diagonalNW;
-	vt<EntityMesh*> diagonalSW;
-	vt<EntityMesh*> diagonalNE;
-	vt<EntityMesh*> diagonalSE;
+	vt<EntityMesh *> ceiling;
+	vt<EntityMesh *> floor;
+	vt<EntityMesh *> pilarEast;
+	vt<EntityMesh *> pilarWest;
+	vt<EntityMesh *> pilarSouth;
+	vt<EntityMesh *> pilarNorth;
+	vt<EntityMesh *> bigaEast;
+	vt<EntityMesh *> bigaWest;
+	vt<EntityMesh *> bigaSouth;
+	vt<EntityMesh *> bigaNorth;
+	vt<EntityMesh *> wallEast;
+	vt<EntityMesh *> wallWest;
+	vt<EntityMesh *> wallSouth;
+	vt<EntityMesh *> wallNorth;
+	vt<EntityMesh *> midCube;
+	vt<EntityMesh *> diagonalNW;
+	vt<EntityMesh *> diagonalSW;
+	vt<EntityMesh *> diagonalNE;
+	vt<EntityMesh *> diagonalSE;
 
-    void loadRooms();
-    void randomizeMap();
+	void loadRooms();
+	void randomizeMap();
 	void placeRoom(int roomID, int roomType);
 	void tryNeighbour(int roomID);
-    void randomLoad();
-    void setTestRooms();
+	void randomLoad();
+	void setTestRooms();
 
-	int countRoomNeighbours(EntityMeshRoom* room);
+	int countRoomNeighbours(EntityMeshRoom *room);
 	void placeRoomsDoors();
-	EntityMeshRoom* getRoom(int x, int y);
+	EntityMeshRoom *getRoom(int x, int y);
 
-    void update();
-    void setCurrentRoom();
+	void update();
+	void setCurrentRoom();
 	void render();
 
 	void setRoomClearStatus(int roomID);
 
 	void initializeBullets(int nBullets = MAX_BULLETS);
-	void shootBullet(Vector3 direction, EntityEnemy* source = NULL);
-	void stashBullet(EntityBullet* bullet);
+	void shootBullet(Vector3 direction, EntityEnemy *source = NULL);
+	void stashBullet(EntityBullet *bullet);
+	bool CheckWinCondition();
+	bool CheckLoseCondition();
 };
